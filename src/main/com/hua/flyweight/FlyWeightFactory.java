b7a47188d17f109f73e8616e4cab537f47ea5aa2
limenghua <limenghua@wondersgroup.com>
2017-11-14 14:06:01 +0800

    享元模式-String和Integer中的使用

--- src/main/com/hua/flyweight/FlyWeightFactory.java
+++ src/main/com/hua/flyweight/FlyWeightFactory.java
@@ -46,3 +46,43 @@ class ClientApplication {
 		System.out.println(String.format("共绘制了%d种颜色的圆形", FlyWeightFactory.getNum()));
 	}
 }
+
+class StringTest {
+	/**
+	 * 字符串常量池是Java节约资源的一种方式。其实就是使用了享元模式的思想
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = "he" + "llo";
+		if (s1 == s2) {
+			System.out.println("两个字符串是一个对象");
+		}
+	}
+}
+
+class IntegerTest {
+	/**
+	 * -128 to 127 会缓存下来，不在这个范围的数值，会新创建一个Integer
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Integer a = 1;
+		Integer b = 1;
+		if (a == b) {
+			System.out.println("1两个整形变量是一个对象");
+		}
+
+		Integer a1 = 127;
+		Integer b1 = 127;
+		if (a1 == b1) {
+			System.out.println("2两个整形变量是一个对象");
+		}
+
+		Integer a2 = 128;
+		Integer b2 = 128;
+		if (a2 == b2) {
+			System.out.println("3两个整形变量是一个对象");
+		}
+	}
+}
