27a2cafe157cc576fcbf72c90d5ec0a91ffdf0b6
limenghua <limenghua@wondersgroup.com>
2018-03-13 18:16:05 +0800

    数据访问对象模式-readme

--- src/main/com/hua/data_access/DataAccessApplication.java
+++ src/main/com/hua/data_access/DataAccessApplication.java
@@ -8,8 +8,35 @@ import java.util.List;
  * @author limenghua
  */
 public class DataAccessApplication {
+
+	public static void main(String[] args) {
+
+		StudentDao studentDao = new StudentDaoImpl();
+		// 输出所有的学生
+		List<Student> students = studentDao.getAllStudents();
+		for (Student student : students) {
+			System.out.println(student);
+		}
+
+		// 更新学生
+		Student student = students.get(0);
+		student.setName("王语嫣");
+		studentDao.updateStudent(student);
+		System.out.println(student);
+
+		// 删除
+		studentDao.delStudent(student);
+		System.out.println("after del");
+		// 输出所有的学生
+		for (Student s : students) {
+			System.out.println(s);
+		}
+	}
 }
 
+/**
+ * 模型对象/数值对象
+ */
 class Student {
 	private int no;
 
@@ -35,18 +62,32 @@ class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return "Student{" +
+				"no=" + no +
+				", name='" + name + '\'' +
+				'}';
+	}
 }
 
+/**
+ * 数据访问对象接口
+ */
 interface StudentDao {
 	List<Student> getAllStudents();
 
-	Student getStudent(String no);
+	Student getStudent(int no);
 
 	void updateStudent(Student student);
 
 	void delStudent(Student student);
 }
 
+/**
+ * 数据访问对象实体类
+ */
 class StudentDaoImpl implements StudentDao {
 
 	// 列表当成一个数据库
@@ -54,29 +95,30 @@ class StudentDaoImpl implements StudentDao {
 
 	static {
 		students = new ArrayList<>();
-		Student student1 = new Student(1001, "汪峰");
-		Student student2 = new Student(1002, "章子怡");
+		Student student1 = new Student(0, "汪峰");
+		Student student2 = new Student(1, "章子怡");
 		students.add(student1);
 		students.add(student2);
 	}
 
 	@Override
 	public List<Student> getAllStudents() {
-		return null;
+		return students;
 	}
 
 	@Override
-	public Student getStudent(String no) {
-		return null;
+	public Student getStudent(int no) {
+		return students.get(no);
 	}
 
 	@Override
 	public void updateStudent(Student student) {
-
+		students.get(student.getNo()).setName(student.getName());
+		System.out.println(String.format("update [%d] name to [%s]", student.getNo(), students.get(student.getNo()).getName()));
 	}
 
 	@Override
 	public void delStudent(Student student) {
-
+		students.remove(student.getNo());
 	}
 }
