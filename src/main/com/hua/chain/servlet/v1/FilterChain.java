fb0c697f2788e9b72d982c05eadad69d6534306d
5820-李孟华 <limenghua@wondersgroup.com>
2021-11-07 14:25:21 +0800

    责任链-Servlet+1

--- src/main/com/hua/chain/servlet/v1/FilterChain.java
+++ src/main/com/hua/chain/servlet/v1/FilterChain.java
@@ -1,9 +1,13 @@
 package com.hua.chain.servlet.v1;
 
 import java.util.ArrayList;
+import java.util.List;
 
-class FilterChain implements Filter {
-	List<Filter> filters = new ArrayList<>();
+/**
+ * 把这些Handler组合起来，变成一个链，并通过一个统一入口处理
+ */
+public class FilterChain implements Filter {
+	private List<Filter> filters = new ArrayList<>();
 
 	public FilterChain add(Filter f) {
 		filters.add(f);
@@ -12,7 +16,6 @@ class FilterChain implements Filter {
 
 	@Override
 	public boolean doFilter(Request request, Response response) {
-
 		for (Filter f : filters) {
 			f.doFilter(request, response);
 		}
