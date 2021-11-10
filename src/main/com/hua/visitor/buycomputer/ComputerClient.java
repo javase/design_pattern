4e653e43b6c79f07eead9f5ea99855ba4c855cd4
5820-李孟华 <limenghua@wondersgroup.com>
2021-11-10 20:32:02 +0800

    访问者模式+1

--- src/main/com/hua/visitor/buycomputer/ComputerClient.java
+++ src/main/com/hua/visitor/buycomputer/ComputerClient.java
@@ -1,6 +1,10 @@
 package com.hua.visitor.buycomputer;
 
-
+/**
+ * 客户端 （Client） 通常会作为集合或其他复杂对象 （例如一个组合树） 的代表。
+ * 客户端通常不知晓所有的具体元素类，
+ * 因为它们会通过抽象接口与集合中的对象进行交互。
+ */
 public class ComputerClient {
 	ComputerPart cpu = new CPU();
 
