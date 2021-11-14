7cd41651eda4ce26eee0e78d5fe30757ddd6451e
5820-李孟华 <limenghua@wondersgroup.com>
2021-11-15 02:13:07 +0800

    浅克隆+1

--- src/main/com/hua/prototype/v1/ShallowClone.java
+++ src/main/com/hua/prototype/v1/ShallowClone.java
@@ -7,13 +7,14 @@ public class ShallowClone {
 	public static void main(String[] args) throws Exception {
 		Person p1 = new Person();
 		Person p2 = (Person) p1.clone();
-		System.out.println(p2.age + " " + p2.score);
-		System.out.println(p2.loc);
+		System.out.printf("克隆对象的age:%s,score:%s\n", p2.age, p2.score);
+		System.out.printf("克隆对象的loc：%s\n", p2.loc);
 
-		System.out.println(p1.loc == p2.loc);
-		// 当原有对象的引用类型成员变量被修改时，克隆对象的这个成员变量会一起被更改
+		System.out.printf("克隆对象和原对象是否==？ %s\n", p1 == p2);
+		System.out.printf("克隆对象的loc和原对象的loc是否==？ %s\n", p1.loc == p2.loc);
+		// 当原有对象（被克隆）的引用类型成员变量被修改时，克隆对象的这个成员变量会一起被更改
 		p1.loc.street = "sh";
-		System.out.println(p2.loc);
+		System.out.printf("原对象的loc被修改后，查看克隆对象的loc：%s\n", p2.loc);
 
 	}
 }
