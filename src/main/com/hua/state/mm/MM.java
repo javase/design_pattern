ffb27d7c2f91351b983eee01e06584075176a74e
5820-李孟华 <limenghua@wondersgroup.com>
2021-11-23 17:37:40 +0800

    状态模式+1

--- src/main/com/hua/state/mm/MM.java
+++ src/main/com/hua/state/mm/MM.java
@@ -1,9 +1,9 @@
 package com.hua.state.mm;
 
 /**
- * 当增加新的状态时非常不方便
+ * 上下文 （Context） 保存了对于一个具体状态对象的引用， 并会将所有与该状态相关的工作委派给它。
+ * 上下文通过状态接口与状态对象交互， 且会提供一个设置器用于传递新的状态对象。
  */
-
 public class MM {
 	private String name;
 
